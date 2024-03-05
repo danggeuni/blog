cfb027c73e93fa8af8f878ecf733136b8d63b473
danggeuni <dydrnxkeh@naver.com>
2024-03-05 14:14:09 +0900

    Initial commit

---
+++ blog/src/test/java/com/miniblog/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniblog.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
